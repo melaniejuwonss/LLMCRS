f422cc8946dfbeabbe94c6023f498ba887ded897
melanie <yjw9700@agape.hanyang.ac.kr>
2023-09-09 12:16:05 +0900

    initial commit

--- preliminary/model.py
+++ preliminary/model.py
@@ -2,32 +2,32 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 
-model_name = "meta-llama/Llama-2-7b-hf"
-model = eval('transformers.AutoModelForCausalLM').from_pretrained(model_name)
+model = "meta-llama/Llama-2-7b-hf"
+# model = eval('transformers.AutoModelForCausalLM').from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model)
 
 input = "I like 'Breaking Bad' and 'Band of Brothers'. Do you have any recommendations of other shows I might like?\n"
-inputs = tokenizer(input)
-output_ids = model.generate(
-            torch.as_tensor(inputs.input_ids).to(model.device),
-            # temperature=temperature,
-            max_new_tokens=128,
+# inputs = tokenizer(input)
+# output_ids = model.generate(
+#             torch.as_tensor(inputs.input_ids).to(model.device),
+#             # temperature=temperature,
+#             max_new_tokens=128,
+# )
+# outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
 )
-outputs = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
-# pipeline = transformers.pipeline(
-#     "text-generation",
-#     model=model,
-#     torch_dtype=torch.float16,
-#     device_map="auto",
-# )
 
-# sequences = pipeline(
-#     'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
-#     do_sample=True,
-#     top_k=10,
-#     num_return_sequences=1,
-#     eos_token_id=tokenizer.eos_token_id,
-#     max_length=200,
-# )
-# for seq in sequences:
-#     print(f"Result: {seq['generated_text']}")
+sequences = pipeline(
+    'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=200,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
