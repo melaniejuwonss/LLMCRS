8fa2b976dfc7a4f4e8e30aec95853943bbfa25e6
melanie <yjw9700@agape.hanyang.ac.kr>
2023-09-09 12:43:05 +0900

    initial commit

--- preliminary/model.py
+++ preliminary/model.py
@@ -20,7 +20,7 @@ pipeline = transformers.pipeline(
     "text-generation",
     model=model,
     torch_dtype=torch.float16,
-    device_map="auto",
+    # device_map="auto",
     use_auth_token=HUGGINGFACE_AUTH_TOKEN
 )
 
