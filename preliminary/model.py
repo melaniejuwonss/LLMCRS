5ad03c923bb347d584412af4b6ca8600d0edb833
melanie <yjw9700@agape.hanyang.ac.kr>
2023-09-09 13:19:15 +0900

    initial commit

--- preliminary/model.py
+++ preliminary/model.py
@@ -18,23 +18,24 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 
-tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-hf", use_auth_token=True)
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
 pipeline = transformers.pipeline(
     "text-generation",
-    model="codellama/CodeLlama-7b-hf",
+    model=model,
     torch_dtype=torch.float16,
     device_map="auto",
 )
 
 sequences = pipeline(
-    'def fibonacci(',
+    'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
     do_sample=True,
-    temperature=0.2,
-    top_p=0.9,
+    top_k=10,
     num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
-    max_length=100,
-    device=0,
+    max_length=200,
+    device=0
 )
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
