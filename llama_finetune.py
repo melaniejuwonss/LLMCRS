f40e62df71f4d0269797ba469b46be6445aa9f62
taeho <hirooms2@hanyang.ac.kr>
2023-09-19 14:44:06 +0900

    requirements.txt

--- llama_finetune.py
+++ llama_finetune.py
@@ -210,7 +210,7 @@ def llama_finetune(
         base_model,
         load_in_8bit=True,
         # load_in_8bit_fp32_cpu_offload=True,
-        # llm_int8_enable_fp32_cpu_offload=True,
+        llm_int8_enable_fp32_cpu_offload=True,
         torch_dtype=torch.float16,
         device_map=device_map,
         # quantization_config=quantization_config,
