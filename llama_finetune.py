d4f5f03e745f50a2069fd555b8ef2f1457194cd0
taeho <hirooms2@hanyang.ac.kr>
2023-09-19 18:12:21 +0900

    requirements.txt

--- llama_finetune.py
+++ llama_finetune.py
@@ -212,11 +212,11 @@ def llama_finetune(
         base_model,
         load_in_8bit=True,
         # load_in_8bit_fp32_cpu_offload=True,
-        llm_int8_enable_fp32_cpu_offload=True,
+        # llm_int8_enable_fp32_cpu_offload=True,
         torch_dtype=torch.float16,
         device_map=device_map,
         # quantization_config=quantization_config,
-    ).to(args.device_id)
+    )
 
     tokenizer.pad_token_id = (
         0  # unk. we want this to be different from the eos token
