3ce6030bc9cce9886f2b13353767f05b54334806
taeho <hirooms2@hanyang.ac.kr>
2023-09-20 21:53:06 +0900

    requirements.txt

--- llama_test.py
+++ llama_test.py
@@ -165,7 +165,7 @@ class LLaMaEvaluator:
                 generated_results.append({'GEN': output, 'ANSWER': label, 'AVG_HIT': hit_ratio})
 
             for i in generated_results:
-                args.log_file.write(json.dumps(i, ensure_ascii=False) + '\n')
+                self.args.log_file.write(json.dumps(i, ensure_ascii=False) + '\n')
             if cnt % 100 == 0 and cnt != 0:
                 print("%.4f" % (hit / cnt))
 
