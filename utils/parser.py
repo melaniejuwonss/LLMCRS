e34d6376fdc5881f82b0641cd98738524fc83361
taeho <hirooms2@hanyang.ac.kr>
2023-11-23 22:50:19 +0900

    231116.  - train 끝내고 test

--- utils/parser.py
+++ utils/parser.py
@@ -34,7 +34,7 @@ def parse_args():
     parser.add_argument('--prompt', type=str, default='withoutCoT')
     parser.add_argument('--data_type', type=str, default='default',
                         choices=['default', 'augment', 'cot', "cot_wo", "cot_p", "cot_s", "cot_sp",
-                                 "cot_onlyp", "cot_onlys", "cot_onlysp", 'lastutt', 'cot_intention'])
+                                 "cot_onlyp", "cot_onlys", "cot_onlysp", 'lastutt', 'cot_intention', 'cot_prompt'])
     parser.add_argument('--isNew', type=bool, default=False)
 
     # ChatGPT
