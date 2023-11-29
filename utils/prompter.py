1332e388043b2992443f96443124b6011b50f2a7
melanie <yjw9700@agape.hanyang.ac.kr>
2023-11-29 17:23:33 +0900

    23.11.29: llama_finetue 수정

--- utils/prompter.py
+++ utils/prompter.py
@@ -57,4 +57,4 @@ class Prompter(object):
         return res
 
     def get_response(self, output: str) -> str:
-        return output.split(self.template["response_split"])[1].strip()
+        return output.split(self.template["response_split"])[-1].strip()
