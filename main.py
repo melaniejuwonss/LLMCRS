dedc136ea7f60bb8720406f16eb7b17ed3b2f347
melanie <yjw9700@agape.hanyang.ac.kr>
2023-12-04 10:49:14 +0900

    23.12.04: only review prompt 수정

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
                 data['context_tokens'] = data['context_tokens'].replace(f"({year})", '')
 
                 data[
-                    'context_tokens'] = f"I will give you a review of movie [BLANK].\n{data['context_tokens']}\n\n Based on the review, guess the movie for [BLANK]."
+                    'context_tokens'] = f"I will give you a review of a movie [BLANK].\n{data['context_tokens']}\n\n Guess which movie is described by the review above."
             train_data = [{'context_tokens': data['context_tokens'], 'item': target_item_list[idx], 'isNew': True} for
                           idx, data in
                           enumerate(train_data)]
