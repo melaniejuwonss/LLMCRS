61716c9f507b9b067d1d8e9ab781f5aee7658978
melanie <yjw9700@agape.hanyang.ac.kr>
2023-12-05 15:24:53 +0900

    23.12.05: review prompt 바꿔보기

--- main.py
+++ main.py
@@ -146,19 +146,19 @@ if __name__ == '__main__':
             target_item_list = [data['item'] for data in train_data]
 
             for idx, data in enumerate(train_data):
-                data['context_tokens'] = data['context_tokens'].replace(target_item_list[idx], '[BLANK]')
+                data['context_tokens'] = data['context_tokens'].replace(target_item_list[idx], '[MOVIE]')
                 title = target_item_list[idx].split('(')[0].strip()
                 year = target_item_list[idx].split('(')[-1][:-1].strip()
                 if not year.isdigit():
                     year = ''
-                data['context_tokens'] = data['context_tokens'].replace(f"\"{title}\" ({year})", '[BLANK]')
-                data['context_tokens'] = data['context_tokens'].replace(f"\"{title.lower()}\" ({year})", '[BLANK]')
-                data['context_tokens'] = data['context_tokens'].replace(title, '[BLANK]')
-                data['context_tokens'] = data['context_tokens'].replace(title.lower(), '[BLANK]')
+                data['context_tokens'] = data['context_tokens'].replace(f"\"{title}\" ({year})", '[MOVIE]')
+                data['context_tokens'] = data['context_tokens'].replace(f"\"{title.lower()}\" ({year})", '[MOVIE]')
+                data['context_tokens'] = data['context_tokens'].replace(title, '[MOVIE]')
+                data['context_tokens'] = data['context_tokens'].replace(title.lower(), '[MOVIE]')
                 data['context_tokens'] = data['context_tokens'].replace(f"({year})", '')
 
                 data[
-                    'context_tokens'] = f"I will give you a review of a movie.\nIn the review, the movie name is masked with [BLANK].\nHere is the review:\n{data['context_tokens']}\n\n Based on the review, guess the movie name for [BLANK] without extra explanations."
+                    'context_tokens'] = f"I will give you a review of a movie.\nIn the review, the movie name is masked with [MOVIE].\nHere is the review:\n{data['context_tokens']}\n\n Based on the review, guess the movie name for [MOVIE] without extra explanations."
             train_data = [{'context_tokens': data['context_tokens'], 'item': target_item_list[idx], 'isNew': True} for
                           idx, data in
                           enumerate(train_data)]
