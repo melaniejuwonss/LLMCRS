1e0bd12fef7e5bbbe2ab88b306c79ddd28f0f8e4
taeho <hirooms2@hanyang.ac.kr>
2023-12-07 16:14:33 +0900

    231207.  - pretrain mode

--- utils/parser.py
+++ utils/parser.py
@@ -44,6 +44,7 @@ def parse_args():
     parser.add_argument('--origin_augment', type=bool, default=False)
     parser.add_argument('--all_merge', type=bool, default=False)
     parser.add_argument('--plot_merge', type=bool, default=False)
+    parser.add_argument('--pretrain', type=bool, default=False)
 
     parser.add_argument('--TH', type=bool, default=False)
     parser.add_argument('--JW', type=bool, default=False)
