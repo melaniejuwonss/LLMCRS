57c3ebd170044bf16a847484373a47887f45be0d
taeho <hirooms2@hanyang.ac.kr>
2024-03-05 10:36:34 +0900

    240226. Return Scores

--- llama_finetune.py
+++ llama_finetune.py
@@ -275,7 +275,7 @@ def llama_finetune(
     model = LlamaForCausalLM.from_pretrained(
         base_model,
         load_in_8bit=True,
-        # load_in_8bit_fp32_cpu_offload=True,
+        load_in_8bit_fp32_cpu_offload=True,
         # llm_int8_enable_fp32_cpu_offload=True,
         torch_dtype=torch.float16,
         device_map=device_map,
