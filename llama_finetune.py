6730a34ef17ab03cc45626ccd376268187e9a6ab
taeho <hirooms2@hanyang.ac.kr>
2024-03-15 09:23:09 +0900

    240314. IA3

--- llama_finetune.py
+++ llama_finetune.py
@@ -9,7 +9,8 @@ from datasets import load_dataset, Dataset
 from transformers import Trainer, TrainingArguments, TrainerState, TrainerControl
 import wandb
 from peft import PeftModel
-# from peft import IA3Config
+
+from trl import SFTTrainer
 
 from utils.parser import parse_args
 
@@ -244,11 +245,121 @@ def llama_finetune(
                                                                     ]  # could be sped up, probably
         return tokenized_full_prompt
 
-    quantization_config = BitsAndBytesConfig(load_in_8bit=True)  # , llm_int8_enable_fp32_cpu_offload=True)
-    # if data_path.endswith(".json") or data_path.endswith(".jsonl"):
-    #     data = load_dataset("json", data_files=data_path)
-    # else:
-    #     data = load_dataset(data_path)
+    ################################################################################
+    # QLoRA parameters
+    ################################################################################
+
+    # LoRA attention dimension
+    lora_r = 64
+
+    # Alpha parameter for LoRA scaling
+    lora_alpha = 16
+
+    # Dropout probability for LoRA layers
+    lora_dropout = 0.1
+
+    ################################################################################
+    # bitsandbytes parameters
+    ################################################################################
+
+    # Activate 4-bit precision base model loading
+    use_4bit = True
+
+    # Compute dtype for 4-bit base models
+    bnb_4bit_compute_dtype = "float16"
+
+    # Quantization type (fp4 or nf4)
+    bnb_4bit_quant_type = "nf4"
+
+    # Activate nested quantization for 4-bit base models (double quantization)
+    use_nested_quant = False
+
+    ################################################################################
+    # TrainingArguments parameters
+    ################################################################################
+
+    # Output directory where the model predictions and checkpoints will be stored
+    output_dir = "./results"
+
+    # Number of training epochs
+    num_train_epochs = 1
+
+    # Enable fp16/bf16 training (set bf16 to True with an A100)
+    fp16 = False
+    bf16 = False
+
+    # # Batch size per GPU for training
+    # per_device_train_batch_size = 4
+    #
+    # # Batch size per GPU for evaluation
+    # per_device_eval_batch_size = 4
+
+    # Number of update steps to accumulate the gradients for
+    gradient_accumulation_steps = 1
+
+    # Enable gradient checkpointing
+    gradient_checkpointing = True
+
+    # Maximum gradient normal (gradient clipping)
+    max_grad_norm = 0.3
+
+    # Initial learning rate (AdamW optimizer)
+    learning_rate = 2e-4
+
+    # Weight decay to apply to all layers except bias/LayerNorm weights
+    weight_decay = 0.001
+
+    # Optimizer to use
+    optim = "paged_adamw_32bit"
+
+    # Learning rate schedule
+    lr_scheduler_type = "cosine"
+
+    # Number of training steps (overrides num_train_epochs)
+    max_steps = -1
+
+    # Ratio of steps for a linear warmup (from 0 to learning rate)
+    warmup_ratio = 0.03
+
+    # Group sequences into batches with same length
+    # Saves memory and speeds up training considerably
+    group_by_length = True
+
+    # Save checkpoint every X updates steps
+    save_steps = 0
+
+    # Log every X updates steps
+    logging_steps = 25
+
+    ################################################################################
+    # SFT parameters
+    ################################################################################
+
+    # Maximum sequence length to use
+    max_seq_length = None
+
+    # Pack multiple short examples in the same input sequence to increase efficiency
+    packing = False
+
+    # Load the entire model on the GPU 0
+    device_map = {"": 0}
+
+    compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=use_4bit,
+        bnb_4bit_quant_type=bnb_4bit_quant_type,
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=use_nested_quant,
+    )
+
+    # Check GPU compatibility with bfloat16
+    if compute_dtype == torch.float16 and use_4bit:
+        major, _ = torch.cuda.get_device_capability()
+        if major >= 8:
+            print("=" * 80)
+            print("Your GPU supports bfloat16: accelerate training with bf16=True")
+            print("=" * 80)
 
     data = []
     if args.prompt == 'DI2E':
@@ -276,14 +387,14 @@ def llama_finetune(
         train_data = data.shuffle().map(generate_and_tokenize_prompt)
         val_data = None
 
-    model = LlamaForCausalLM.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         base_model,
-        # load_in_8bit=True,
-        # llm_int8_enable_fp32_cpu_offload=True,
-        torch_dtype=torch.float16,
         device_map=device_map,
-        quantization_config=quantization_config,
+        quantization_config=bnb_config,
     )
+
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
 
     tokenizer.pad_token_id = (
         0  # unk. we want this to be different from the eos token
@@ -292,11 +403,11 @@ def llama_finetune(
 
     model = prepare_model_for_int8_training(model)
 
-    config = LoraConfig(
-        r=lora_r,
+    # Load LoRA configuration
+    peft_config = LoraConfig(
         lora_alpha=lora_alpha,
-        target_modules=lora_target_modules,
         lora_dropout=lora_dropout,
+        r=lora_r,
         bias="none",
         task_type="CAUSAL_LM",
     )
@@ -309,7 +420,7 @@ def llama_finetune(
     #     )
     # else:
     #     model = get_peft_model(model, config)
-    model = get_peft_model(model, config)
+    # model = get_peft_model(model, config)
 
     if resume_from_checkpoint[resume_from_checkpoint.rfind('/') + 1:] != "lora-alpaca":
         # Check the available weights and load them
@@ -339,47 +450,81 @@ def llama_finetune(
         model.is_parallelizable = True
         model.model_parallel = True
 
-    trainer = Trainer(
+    # Set training parameters
+    training_arguments = TrainingArguments(
+        output_dir=output_dir,
+        num_train_epochs=num_train_epochs,
+        per_device_train_batch_size=per_device_train_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        optim=optim,
+        save_steps=save_steps,
+        logging_steps=logging_steps,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        fp16=fp16,
+        bf16=bf16,
+        max_grad_norm=max_grad_norm,
+        max_steps=max_steps,
+        warmup_ratio=warmup_ratio,
+        group_by_length=group_by_length,
+        lr_scheduler_type=lr_scheduler_type,
+        report_to="wandb" if use_wandb else None,
+    )
+
+    # Set supervised fine-tuning parameters
+    trainer = SFTTrainer(
         model=model,
         train_dataset=train_data,
-        eval_dataset=val_data,
-        args=transformers.TrainingArguments(
-            per_device_train_batch_size=per_device_train_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps,
-            warmup_steps=warmup_steps,
-            num_train_epochs=num_epochs,
-            learning_rate=learning_rate,
-            fp16=True,
-            logging_steps=10,
-            optim="adamw_torch",
-            evaluation_strategy="steps" if val_set_size > 0 else "no",
-            save_strategy="steps",
-            eval_steps=5 if val_set_size > 0 else None,
-            save_steps=200,
-            output_dir=output_dir,
-            save_total_limit=3,
-            load_best_model_at_end=True if val_set_size > 0 else False,
-            ddp_find_unused_parameters=False if ddp else None,
-            group_by_length=group_by_length,
-            report_to="wandb" if use_wandb else None,
-            # run_name=args.wandb_run_name if use_wandb else None,
-        ),
-        data_collator=transformers.DataCollatorForSeq2Seq(
-            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
-        ),
+        peft_config=peft_config,
+        dataset_text_field="text",
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args=training_arguments,
+        packing=packing,
         callbacks=[QueryEvalCallback(args, evaluator)]
     )
-    model.config.use_cache = False
 
-    old_state_dict = model.state_dict
-    model.state_dict = (
-        lambda self, *_, **__: get_peft_model_state_dict(
-            self, old_state_dict()
-        )
-    ).__get__(model, type(model))
+    # trainer = Trainer(
+    #     model=model,
+    #     train_dataset=train_data,
+    #     eval_dataset=val_data,
+    #     args=transformers.TrainingArguments(
+    #         per_device_train_batch_size=per_device_train_batch_size,
+    #         gradient_accumulation_steps=gradient_accumulation_steps,
+    #         warmup_steps=warmup_steps,
+    #         num_train_epochs=num_epochs,
+    #         learning_rate=learning_rate,
+    #         fp16=True,
+    #         logging_steps=10,
+    #         optim="adamw_torch",
+    #         evaluation_strategy="steps" if val_set_size > 0 else "no",
+    #         save_strategy="steps",
+    #         eval_steps=5 if val_set_size > 0 else None,
+    #         save_steps=200,
+    #         output_dir=output_dir,
+    #         save_total_limit=3,
+    #         load_best_model_at_end=True if val_set_size > 0 else False,
+    #         ddp_find_unused_parameters=False if ddp else None,
+    #         group_by_length=group_by_length,
+    #         report_to="wandb" if use_wandb else None,
+    #         # run_name=args.wandb_run_name if use_wandb else None,
+    #     ),
+    #     data_collator=transformers.DataCollatorForSeq2Seq(
+    #         tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+    #     ),
+    #     callbacks=[QueryEvalCallback(args, evaluator)]
+    # )
+    # model.config.use_cache = False
 
-    if torch.__version__ >= "2" and sys.platform != "win32":
-        model = torch.compile(model)
+    # old_state_dict = model.state_dict
+    # model.state_dict = (
+    #     lambda self, *_, **__: get_peft_model_state_dict(
+    #         self, old_state_dict()
+    #     )
+    # ).__get__(model, type(model))
+    #
+    # if torch.__version__ >= "2" and sys.platform != "win32":
+    #     model = torch.compile(model)
 
     trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
